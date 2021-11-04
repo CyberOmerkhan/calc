a74b5b9afad549c2da829dcf2fba63293ccc294e
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-04 19:55:12 +0600

    final version v1.0

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ let input = document.querySelector('input');
 function insert(num){
     input.value += num;
 }
+function back(){
+    input.value = input.value.substr(0, input.value.length - 1);
+}
+function clean(){
+    input.value = '';
+}
+function showResult(){
+    input.value = eval(input.value);
+}
